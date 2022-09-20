e02664cd69f3e510698198643f0c99dc13a265ba
raven250 <67653646+raven250@users.noreply.github.com>
2022-09-20 19:12:55 +0100

    added api project

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HousepartyConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "houseparty"
+    name = "api"
